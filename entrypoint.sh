7bf2e3f3301a35cec2a964d3c1384db64e0a9fcd
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2025-01-12 03:19:57 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,14 +11,13 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-output="$(actionlint -oneline ${INPUT_ACTIONLINT_FLAGS})"
-error_level="$(echo "$output" | sed -e 's/^.* shellcheck reported issue in this script: [^:]*:\([^:]*\):.*$/\1/g')"
-echo "$error_level"
-echo sed -e "s/^\([^:]*:[^:]*:[^:]*:\) \(.*\)\$/\1/g"
-echo "$output" | sed -e "s/^\([^:]*:[^:]*:[^:]*:\) \(.*\)\$/\1/g"
+output="$(for r in $(actionlint -oneline ${INPUT_ACTIONLINT_FLAGS}); do
+  error_level="$(echo "$r" | sed -e 's/^.* shellcheck reported issue in this script: [^:]*:\([^:]*\):.*$/\1/g')"
+  echo "$r" | sed -e "s/^\([^:]*:[^:]*:[^:]*:\) \(.*\)\$/\1$error_level \2/g"
+done)"
 
 # shellcheck disable=SC2086
-echo "$output" | sed -e "s/^\([^:]*:[^:]*:[^:]*:\) \(.*\)\$/\1$error_level \2/g" \
+echo "$output" \
     | reviewdog \
         -efm="%f:%l:%c:%t %m" \
         -name="${INPUT_TOOL_NAME}" \

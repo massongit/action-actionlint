b89da2e597943511e1ee15a560bf7fa89c72f69c
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2025-01-11 09:56:06 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,16 +12,7 @@ fi
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 # shellcheck disable=SC2086
-actionlint -oneline ${INPUT_ACTIONLINT_FLAGS} \
-    | reviewdog \
-        -efm="%f:%l:%c: %m" \
-        -name="${INPUT_TOOL_NAME}" \
-        -reporter="${INPUT_REPORTER}" \
-        -filter-mode="${INPUT_FILTER_MODE}" \
-        -fail-level="${INPUT_FAIL_LEVEL}" \
-        -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-        -level="${INPUT_LEVEL}" \
-        ${INPUT_REVIEWDOG_FLAGS}
+actionlint -oneline ${INPUT_ACTIONLINT_FLAGS}
 exit_code=$?
 
 exit $exit_code

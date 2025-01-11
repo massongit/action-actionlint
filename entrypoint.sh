d35ef516c9b616aa6f8d9a07d166af067dc1d152
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2025-01-12 03:24:19 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-output="$(for r in $(actionlint -oneline ${INPUT_ACTIONLINT_FLAGS}); do
+output="$(actionlint -oneline ${INPUT_ACTIONLINT_FLAGS} | while read r; do
   error_level="$(echo "$r" | sed -e 's/^.* shellcheck reported issue in this script: [^:]*:\([^:]*\):.*$/\1/g')"
   echo "$r" | sed -e "s/^\([^:]*:[^:]*:[^:]*:\) \(.*\)$/\1$error_level \2/g"
 done)"

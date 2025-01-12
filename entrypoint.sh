ee319169ef7de3dcedcf3b5a70da07d7dd6300d0
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2025-01-12 10:21:03 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,15 +10,13 @@ if [ -n "${GITHUB_WORKSPACE}" ] ; then
 fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-
-raw_output="$(actionlint -oneline ${INPUT_ACTIONLINT_FLAGS})"
 output=""
+
 while read r; do
   error_level="$(echo "${r}" | sed -e 's/^.* shellcheck reported issue in this script: [^:]*:\([^:]\)[^:]*:.*$/\1/g')"
   output="${output}$(echo "${r}" | sed -e "s/^\([^:]*:[^:]*:[^:]*:\) \(.*\)$/\1${error_level} \2/g")\n"
-done < <(echo "${raw_output}")
+done < <(actionlint -oneline ${INPUT_ACTIONLINT_FLAGS})
 
-# shellcheck disable=SC2086
 echo -e "${output}" \
     | reviewdog \
         -efm="%f:%l:%c:%t %m" \
